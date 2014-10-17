efd9f5b44179383edb963cf20adea179ecd16dcf
ljn <ljn@ljn-PC.(none)>
2014-10-17 13:57:50 +0800

    <ljn>

---
+++ Project/src/com/minor/water/bean/CityModel.java
@@ -0,0 +1,33 @@
+package com.minor.water.bean;
+
+/**
+ * 城市属�?实体�?
+ * @author sy
+ *
+ */
+public class CityModel
+{
+	private String CityName; //城市名字
+	private String NameSort; //城市首字�?
+
+	public String getCityName()
+	{
+		return CityName;
+	}
+
+	public void setCityName(String cityName)
+	{
+		CityName = cityName;
+	}
+
+	public String getNameSort()
+	{
+		return NameSort;
+	}
+
+	public void setNameSort(String nameSort)
+	{
+		NameSort = nameSort;
+	}
+
+}
